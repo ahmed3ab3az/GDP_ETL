f5305928661ebd5b0428ed55c876022d797194b3
Ahmed Abdelaziz <ahmed3ab3az@gmail.com>
2024-08-05 15:01:27 +0300

    init

---
+++ etl_code.py
@@ -0,0 +1,57 @@
+import glob 
+import pandas as pd 
+import xml.etree.ElementTree as ET 
+from datetime import datetime 
+
+log_file = "log_file.txt" 
+target_file = "transformed_data.csv" 
+
+def extract_from_csv(file_to_process): 
+    dataframe = pd.read_csv(file_to_process) 
+    return dataframe 
+def extract_from_json(file_to_process): 
+    dataframe = pd.read_json(file_to_process) 
+    return dataframe 
+def extract_from_xml(file_to_process): 
+    dataframe = pd.DataFrame(columns=['name' , "height", "weight"])
+    tree = ET.ElementTree(file_to_process)
+    root = tree.getroot()
+    for row in root:
+        name = row.find('name')
+        height = float(row.find('height'))
+        weight = float(row.find('weight'))
+        dataframe = pd.concat([dataframe, pd.DataFrame({'name' : name , 'height' : height , 'weight' : weight })], ignore_index=True)
+    return dataframe 
+def extract():
+    extraced_data = dataframe = pd.DataFrame(columns=['name' , "height", "weight"])
+
+    # process all csv files
+    for file in glob.glob('*.csv'):
+        extraced_data = pd.concat([extraced_data , extract_from_csv(file)] , ignore_index=True)
+
+
+    # process all json files
+    for file in glob.glob('*.json'):
+        extraced_data = pd.concat([extraced_data , extract_from_json(file)] , ignore_index=True)
+
+    # process all csv files
+    for file in glob.glob('*.xml'):
+        extraced_data = pd.concat([extraced_data , extract_from_xml(file)] , ignore_index=True)
+    return extraced_data
+def transform(data):
+    '''Convert inches to meters and round off to two decimals 
+    1 inch is 0.0254 meters '''
+    data['height'] = round(data['height'] * 0.0254 , 2)
+    '''Convert pounds to kilograms and round off to two decimals 
+    1 pound is 0.45359237 kilograms '''
+    data['weight'] = round(data.weight * 0.45359237,2) 
+    return data
+def load_data(target_file , transformed_data):
+    transformed_data.to_csv(target_file)
+def log_progress(message):
+    timestamp_form = '%Y-%h-%d-%H:%M:%S'
+    now = datetime.now()
+    timestamp = now.strftime(timestamp_form)
+    with open(log_file,'a') as log:
+        log.write(timestamp + ' , ' + message + '\n')
+
